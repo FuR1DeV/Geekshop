6896b3e028a993803ef1d5cc569b76c85000241b
vasia19rus <vasia19rus@bk.ru>
2021-04-21 23:48:43 +0700

    Lesson_1

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
-
+# Lesson_1
 # Create your views here.
 # функции = контроллеры = вьюхи
+
 
 def index(request):
     return render(request, 'mainapp/index.html')
