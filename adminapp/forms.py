55cb56dcd2fefcd640824860fbacc75cd6d95ecb
vasia19rus <vasia19rus@bk.ru>
2021-05-22 19:43:26 +0700

    added update users and fix error in admin_staff

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from authapp.forms import UserRegisterForm
+from authapp.forms import UserRegisterForm, UserProfileForm
 from authapp.models import User
 
 
@@ -9,3 +9,8 @@ class UserAdminRegisterForm(UserRegisterForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password2')
+
+
+class UserAdminProfileForm(UserProfileForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4'}))
